9a3d4b52b7de5b6de79c934e692cf5ef405dd414
adigitoleo <adigitoleo@dissimulo.com>
2021-09-27 18:50:53 +1000

    Add audacity floating rule

--- .config/dwl/config.h
+++ .config/dwl/config.h
@@ -31,8 +31,9 @@ static const Rule rules[] = {
     { "inkscape", NULL, 0, 1, -1 },
     { "mscore", NULL, 0, 1, -1 },
     { "mypaint", NULL, 0, 1, -1, 360, 100, 1200, 880 },
-    { "libreoffice", NULL, 0, 1, -1, 360, 100, 1200, 880},
-    { "gov-nasa-giss-panoply-Panoply", NULL, 0, 1, -1},
+    { "libreoffice", NULL, 0, 1, -1, 360, 100, 1200, 880 },
+    { "gov-nasa-giss-panoply-Panoply", NULL, 0, 1, -1 },
+    { "audacity", NULL, 0, 1, -1, 360, 100, 1200, 880 },
     { "matplotlib", NULL, 0, 1, -1 },  /* FIXME: not working */
 };
 
