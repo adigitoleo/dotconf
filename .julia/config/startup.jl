8062fb7cc1b51689cef0efe634201e0513ea2d84
adigitoleo <adigitoleo@dissimulo.com>
2021-10-21 15:26:04 +1100

    Add a few utility functions to startup.jl

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,4 +1,5 @@
 # Reduce color space of most REPL components from 16 to 8.
+# Line numbers is stacktraces are still unreadable in dark themes...
 Base.text_colors[:white] = Base.text_colors[:yellow]
 Base.text_colors[:light_cyan] = Base.text_colors[:cyan]
 Base.text_colors[:light_red] = Base.text_colors[:red]
@@ -8,10 +9,17 @@ Base.text_colors[:light_black] = Base.text_colors[:normal]
 Base.text_colors[:light_blue] = Base.text_colors[:blue]
 Base.text_colors[:light_green] = Base.text_colors[:green]
 
-# Line numbers is stacktraces are still unreadable in dark themes...
+"""List available named colors from Base.colors."""
+function colornames()
+    for key in keys(Base.text_colors)
+        if isa(key, Symbol)
+            println(key)
+        end
+    end
+end
 
-# for key in keys(Base.text_colors)
-#     if isa(key, Symbol)
-#         println(key)
-#     end
-# end
+
+"""List modules loaded into m with the `using` keyword."""
+function modules(m::Module)
+    return ccall(:jl_module_usings, Any, (Any,), m)
+end
