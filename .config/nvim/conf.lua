f4605d42faaceff868d185b7ba15caee6d830863
adigitoleo <adigitoleo@dissimulo.com>
2023-07-28 16:03:28 +1000

    Use different color for todo-comments.nvim FIXME highlights

--- .config/nvim/conf.lua
+++ .config/nvim/conf.lua
@@ -50,7 +50,7 @@ vim.keymap.set('n', 'mr', minimap.refresh)
 vim.keymap.set('n', 'gm', minimap.toggle)
 
 -- TODO/FIXME comment tracker setup.
-todo_comments = require('todo-comments').setup({signs = false})
+todo_comments = require('todo-comments').setup({signs = false, colors = {error = {"DiagnosticWarn"}}})
 vim.keymap.set("n", "]t", function()
     todo_comments.jump_next()
 end, { desc = "Next todo comment" })
