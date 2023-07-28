d3ab1af993b815e4ba4a94478d7900dbdd11a482
adigitoleo <adigitoleo@posteo.net>
2023-07-29 07:55:13 +1000

    Use lua-language-server if available

--- .config/nvim/conf.lua
+++ .config/nvim/conf.lua
@@ -19,6 +19,32 @@ if has_lspconfig then
         }
     end
 
+    -- https://github.com/LuaLS/lua-language-server
+    if vim.fn.executable('lua-language-server') > 0 then
+        lsp.lua_ls.setup {
+            settings = {
+                Lua = {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                        version = 'LuaJIT',
+                    },
+                    diagnostics = {
+                        -- Get the language server to recognize the `vim` global
+                        globals = {'vim'},
+                    },
+                    workspace = {
+                        -- Make the server aware of Neovim runtime files
+                        library = vim.api.nvim_get_runtime_file("", true),
+                    },
+                    -- Do not send telemetry data containing a randomized but unique identifier
+                    telemetry = {
+                        enable = false,
+                    },
+                },
+            },
+        }
+    end
+
     -- LSP mappings and autocommands.
     -- To focus the hover buffer, press K a second time.
     vim.api.nvim_create_autocmd('LspAttach', {
