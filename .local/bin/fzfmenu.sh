a25c7a9a5a8f7e76926ba546abdb1df5d22e1500
adigitoleo <adigitoleo@posteo.net>
2023-08-05 07:07:11 +1000

    Migrate from dex to gio launch for desktop file execution

--- .local/bin/fzfmenu.sh
+++ .local/bin/fzfmenu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 helpf() {
-    echo 'Select and run a desktop application using dex(1).'
+    echo 'Select and run a desktop application using gio(1).'
     echo 'The application list is built using `.desktop` files from'
     echo '`/usr/share/applications`. Also requires find(1) and fzf(1).'
 }
@@ -12,16 +12,9 @@ while getopts "h" OPT; do
     esac
 done
 
-if 1>/dev/null 2>&1 command -v dex;  then
-    # TODO: Preview window with app name?
-    # Unfortunately, the xdg-dekstop spec is spectacularly stupid,
-    # and forces us to parse the whole .desktop file to find even simple info:
-    # %zsh: grep '^Name=' /usr/share/applications/org.qutebrowser.qutebrowser.desktop
-    # Name=qutebrowser
-    # Name=New Window
-    # Name=Preferences
+if 1>/dev/null 2>&1 command -v gio;  then
     find /usr/share/applications -type f -name '*.desktop'|fzf --height 100% \
-        --bind "enter:execute(setsid dex {})+abort"
+        --bind "enter:execute(setsid -f gio launch {})+abort"
 else
-    >&2 printf '%s\n' 'Requires dex, https://github.com/jceb/dex'
+    >&2 printf '%s\n' 'Requires `gio launch` from https://gitlab.gnome.org/GNOME/glib/'
 fi
