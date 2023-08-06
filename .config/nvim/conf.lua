8074c02209f8f26d07d46e5f7f5f14ea831c0fb7
adigitoleo <adigitoleo@posteo.net>
2023-08-06 19:11:19 +1000

    Disable environment emulation for LuaLSP

--- .config/nvim/conf.lua
+++ .config/nvim/conf.lua
@@ -35,6 +35,8 @@ if has_lspconfig then
                     workspace = {
                         -- Make the server aware of Neovim runtime files
                         library = vim.api.nvim_get_runtime_file("", true),
+                        -- silence weird luassert message, https://github.com/LuaLS/lua-language-server/discussions/1688
+                        checkThirdParty = false,
                     },
                     -- Do not send telemetry data containing a randomized but unique identifier
                     telemetry = {
