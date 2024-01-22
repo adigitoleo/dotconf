c701e72b9c35a18b1233ef842b231ae401ef7aa1
adigitoleo <adigitoleo@posteo.net>
2024-01-22 17:36:37 +1100

    feat: Use basename(1) to turn .desktop file paths into app names

--- .local/bin/fzfmenu.sh
+++ .local/bin/fzfmenu.sh
@@ -13,7 +13,8 @@ while getopts "h" OPT; do
 done
 
 if 1>/dev/null 2>&1 command -v gio;  then
-    find /usr/share/applications -type f -name '*.desktop'|fzf --height 100% \
+    find /usr/share/applications -type f -name '*.desktop'| \
+        while IFS= read -r line; do basename -s .desktop "$line"; done|fzf --height 100% \
         --bind "enter:execute(setsid -f gio launch {})+abort"
 else
     >&2 printf '%s\n' 'Requires `gio launch` from https://gitlab.gnome.org/GNOME/glib/'
