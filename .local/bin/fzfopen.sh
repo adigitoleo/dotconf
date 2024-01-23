31db32062d0e78b633cc4d37e7a89ee6ed33cbf4
adigitoleo <adigitoleo@posteo.net>
2024-01-23 11:12:44 +1100

    fix: Restore globbing of PDF files

--- .local/bin/fzfopen.sh
+++ .local/bin/fzfopen.sh
@@ -14,6 +14,6 @@ while getopts "h" OPT; do
     esac
 done
 
-rg --files --hidden --binary --type-not=all --no-messages --no-ignore-vcs| \
+rg --files --hidden --binary --type-clear=pdf --type-not=all --no-messages --no-ignore-vcs| \
     fzf --height 100% --bind "enter:execute(setsid xdg-open {} 2>/dev/null &)+abort" \
     --preview "fzfpreview.sh {}" --preview-window "down:60%:sharp"
